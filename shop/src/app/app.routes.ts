662da5a0f06ac7e985727d1fa9ed4d2ce15301d3
Vick <Vickprestone20@gmail.com>
2024-09-25 16:31:35 +0300

    feat(app): initial commit for user login and registration feature
    Signed-off-by: Vick <Vickprestone20@gmail.com>

--- shop/src/app/app.routes.ts
+++ shop/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LayoutComponent,
+        children: [
+            {path: '', component: LoginComponent},
+            {path: 'register', component: RegisterComponent}
+        ]
+    }
+];
